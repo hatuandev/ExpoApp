f4f37d453cf13e548ddf98215c153335bbc7d884
Your Name <you@example.com>
2024-04-15 15:47:34 +0700

    update tabs

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,9 +1,9 @@
-import { View, Text } from 'react-native'
+import { Text, View } from 'react-native';
 import React from 'react'
 
 export default function HomeScreen() {
   return (
-    <View>
+    <View className="flex-1 items-center justify-center bg-white">
       <Text>HomeScreen</Text>
     </View>
   )
