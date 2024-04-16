45858025c0f98911b013d1630ac8ab371710c5c6
kimori01 <anhtuan1991qn@gmail.com>
2024-04-17 00:09:27 +0700

    update

--- src/component/HomeScreens/Header.tsx
+++ src/component/HomeScreens/Header.tsx
@@ -1,22 +1,29 @@
 import { View, Text, TextInput } from "react-native";
 import React from "react";
-import { Ionicons } from "@expo/vector-icons";
+import { Feather, Ionicons, Fontisto } from "@expo/vector-icons";
 import TextPrimary from "../Texts/TextPrimary";
 
 export default function Header() {
-  const [value, setValue] = React.useState("");
-
   return (
-    <View>
-      <View className="flex flex-row items-center gap-2">
-        <Ionicons name="person-circle" size={46} color="black" />
-        <View>
-          <Text>User Name</Text>
-          <Text>User Name</Text>
+    <View className="p-3 pt-10">
+      <View className="flex flex-row items-center justify-between">
+        <View className="flex flex-row items-center gap-1">
+          <Ionicons name="person-circle" size={46} color="white" />
+          <View>
+            <Text className="font-bold text-blue-500">Ho Anh Tuan</Text>
+            <Text className="font-normal text-blue-500">User Name</Text>
+          </View>
         </View>
+        <Fontisto name="bell" size={28} color="white" />
       </View>
-      <View>
-        <TextPrimary />
+      <View className="relative flex flex-row items-center justify-start mt-4 mb-1">
+        <TextInput
+          placeholder="Tìm kiếm"
+          className="absolute p-1 pl-8 bg-white border border-white text-gray-900 text-lg rounded-xl w-full h-9"
+        />
+        <View className="pl-2">
+          <Feather name="search" size={20} color="black" />
+        </View>
       </View>
     </View>
   );
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,11 +1,11 @@
-import { Text, View } from 'react-native';
-import React from 'react'
-import Header from '@/component/HomeScreens/Header';
+import { Text, View } from "react-native";
+import React from "react";
+import Header from "@/component/HomeScreens/Header";
 
 export default function HomeScreen() {
   return (
-    <View className="py-8 px-8 bg-white">
+    <View className="bg-blue-200">
       <Header />
     </View>
-  )
+  );
 }
